c0ed25936033c26a7ba6ad55b2fb4bc06be2aa33
Nattawut Phetmak <neizod@gmail.com>
2012-12-14 16:39:02 +0700

    accept multiline-statement

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP, NEWLINE
+from tokenize import tokenize, untokenize, TokenError, NUMBER, NAME, OP, NL
 from io import BytesIO
 
 import builtins
@@ -9,42 +9,54 @@ import callnum
 
 __all__ = ['interact']
 
-# hook into builtins
-builtins.real = lambda x: type(x)(x.real)
-builtins.imag = lambda x: type(x)(x.imag)
-builtins.conjugate = lambda x: type(x)(x.conjugate()) # XXX
-# conjugate int, float -> self // complex -> complex
+def hook_builtins(func):
+    setattr(builtins, func.__name__, func)
+
+@hook_builtins
+def real(x):
+    return type(x)(x.real)
+
+@hook_builtins
+def imag(x):
+    return type(x)(x.imag)
+
+@hook_builtins
+def conjugate(x):
+    return type(x)(x.conjugate())
 
 def numeric_type(tokval):
     tokval = tokval.lower()
     return 'complex' if 'j' in tokval else 'float' if '.' in tokval else 'int'
 
 def hook_callable_number(string):
-    # since Untokenize.compat() ignore INDENT, unless there exists NEWLINE.
-    # initialize result with NEWLINE is a must. (then remove it at returns).
-    result = [(NEWLINE, '\n')]
-    g = tokenize(BytesIO(string.encode('utf-8')).readline)
-    for toknum, tokval, _, _, _  in g:
-        if toknum == NUMBER:
-            result.extend([ (NAME, 'callnum'),
-                            (OP, '.'),
-                            (NAME, numeric_type(tokval)),
-                            (OP, '('),
-                            (NUMBER, tokval),
-                            (OP, ')'), ])
-        else:
-            result.append((toknum, tokval))
+    # since Untokenize.compat() ignore INDENT, unless there exists NL.
+    # initialize result with NL is a must. (then remove it at returns).
+    result = [(NL, '\n')]
+    token_sequence = tokenize(BytesIO(string.encode('utf-8')).readline)
+    try:
+        for toknum, tokval, _, _, _  in token_sequence:
+            if toknum == NUMBER:
+                result.extend([ (NAME, 'callnum'),
+                                (OP, '.'),
+                                (NAME, numeric_type(tokval)),
+                                (OP, '('),
+                                (NUMBER, tokval),
+                                (OP, ')'), ])
+            else:
+                result.append((toknum, tokval))
+    except TokenError:
+        pass
     return untokenize(result).decode('utf-8').replace('\n', '', 1)
 
 def input_with_callable_number(prompt=''):
     return hook_callable_number(input(prompt))
 
-def interact( banner   = 'calculator the right way',
+def interact( banner   = 'Calculator the Right Way',
               readfunc = input_with_callable_number,
-              local    = { '__name__': '__calculator__',
-                           '__doc__':  None,
+              local    = { '__name__':      '__calculator__',
+                           '__doc__':       None,
                             '__builtins__': builtins,
-                           'callnum':  callnum, }, ):
+                           'callnum':       callnum, }, ):
     ''' Start interactive Python-base calculator. '''
     from code import interact
     import readline
